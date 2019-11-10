98a9448509a85651716caa33b29690d455e98aec
bchornii <bogdan.chorniy@gmail.com>
2019-11-10 21:36:28 +0200

    Changed css structure to fit 7-1 pattern

--- src/Spa/src/app/home/home.component.ts
+++ src/Spa/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account/account.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private authSubscription: Subscription;
+  private isAuthenticated = false;
+
+  constructor(private accountService: AccountService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.authSubscription = this.accountService.isUserAuthenticated$
+      .subscribe(r => this.isAuthenticated = r.isAuthenticated);
+  }
+
+  getStarted() {
+    this.isAuthenticated
+      ? this.router.navigate(['/home'])
+      : this.router.navigate(['/account/login']);
   }
 
 }
