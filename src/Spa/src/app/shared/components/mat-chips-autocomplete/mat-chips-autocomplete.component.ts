796d54bea047034358ca0dafbe7d07c9fef56297
bchornii <bogdan.chorniy@gmail.com>
2019-12-09 00:15:32 +0200

    Added chips autocomplete component

---
+++ src/Spa/src/app/shared/components/mat-chips-autocomplete/mat-chips-autocomplete.component.ts
@@ -0,0 +1,100 @@
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  Self,
+  Input,
+  AfterViewInit,
+  ViewChild,
+  EventEmitter,
+  Output,
+  OnDestroy
+} from '@angular/core';
+import { MatFormElement } from '../mat-form-element';
+import { NgControl } from '@angular/forms';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+
+@Component({
+  selector: 'app-mat-chips-autocomplete',
+  templateUrl: './mat-chips-autocomplete.component.html'
+})
+export class MatChipsAutocompleteComponent extends MatFormElement implements OnInit, OnDestroy, AfterViewInit {
+  @Input() name = 'item';
+  @Input() label: string;
+  @Input() readonly = false;
+  @Input() removable = true;
+  @Input() debounceTime = 300;
+  @Input() isLoading = false;
+  @Input() options: any[];
+  @Input() data: any[];
+
+  @ViewChild('input', { static: false }) input: ElementRef;
+  @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
+
+  @Output('blur') onBlurChange = new EventEmitter<Event>();
+  @Output('focus') onFocusChange = new EventEmitter<Event>();
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  constructor(public elementRef: ElementRef,
+              @Self() public ngCrtl: NgControl) {
+    super(ngCrtl);
+  }
+
+  ngOnInit() {
+    this.setElementId();
+  }
+
+  ngOnDestroy() {
+    this.valueChangesSubstription.unsubscribe();
+  }
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.focus = () => {
+      this.input.nativeElement.focus();
+    };
+  }
+
+  registerOnChange(fn) {
+    this.valueChangesSubstription =
+      this.inputControl.valueChanges.pipe(
+        debounceTime(this.debounceTime),
+        distinctUntilChanged()
+      ).subscribe(fn);
+  }
+
+  add(event: MatChipInputEvent): void {
+    if (!this.matAutocomplete.isOpen) {
+      const value = event.value;
+      const input = event.input;
+
+      if ((value || '').trim()) {
+        this.data.push(value.trim());
+      }
+
+      if (input) {
+        input.value = '';
+      }
+
+      this.inputControl.setValue(null, { emitEvent: false});
+    }
+  }
+
+  remove(item: string): void {
+    const index = this.data.indexOf(item);
+
+    if (index >= 0) {
+      this.data.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.data.push(event.option.viewValue);
+    this.input.nativeElement.value = '';
+    this.inputControl.setValue(null, { emitEvent: false});
+  }
+
+}
