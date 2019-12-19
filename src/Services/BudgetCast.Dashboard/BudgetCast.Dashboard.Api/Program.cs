3ce408f4cefc6a09b91ba042799246dadacc931c
bchornii <bogdan.chorniy@gmail.com>
2019-12-20 00:29:54 +0200

    Added az keyvault

--- src/Services/BudgetCast.Dashboard/BudgetCast.Dashboard.Api/Program.cs
+++ src/Services/BudgetCast.Dashboard/BudgetCast.Dashboard.Api/Program.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace BudgetCast.Dashboard.Api
 {
@@ -22,8 +26,25 @@ namespace BudgetCast.Dashboard.Api
             }            
         }
 
+        https://docs.microsoft.com/en-us/aspnet/core/security/key-vault-configuration?view=aspnetcore-2.2
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        var builtConfig = config.Build();
+
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                        config.AddAzureKeyVault(
+                            $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                            keyVaultClient, new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .UseStartup<Startup>();
     }
 }
