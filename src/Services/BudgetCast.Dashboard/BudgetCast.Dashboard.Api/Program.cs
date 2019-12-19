812a28e7b443942939a9edcb605e504adaf9051e
bchornii <bogdan.chorniy@gmail.com>
2019-12-19 23:35:28 +0200

    Added automatic migrations for database on app startup

--- src/Services/BudgetCast.Dashboard/BudgetCast.Dashboard.Api/Program.cs
+++ src/Services/BudgetCast.Dashboard/BudgetCast.Dashboard.Api/Program.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore;
+﻿using BudgetCast.Dashboard.Api.Extensions;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace BudgetCast.Dashboard.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            try
+            {
+                CreateWebHostBuilder(args).Build()
+                    .MigrateDbContext<IdentityDbContext>((_, __) => { })
+                    .Run();                
+                return 0;
+            }
+            catch
+            {
+                return 1;                
+            }            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
