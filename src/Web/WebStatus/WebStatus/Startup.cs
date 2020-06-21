57f88b079b34dbcb5f1a5ec85b0f1677641e0663
bchornii <bogdan.chorniy@gmail.com>
2020-06-22 00:19:32 +0300

    WebStatus: added simple ui project to monitor health of app's microservices

---
+++ src/Web/WebStatus/WebStatus/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace WebStatus
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy());
+
+            var maximumHistoryEntriesPerEndpoint = Convert.ToInt32(
+                Configuration["MaximumHistoryEntriesPerEndpoint"]);           
+            services
+                .AddHealthChecksUI(setup =>
+                {
+                    setup.MaximumHistoryEntriesPerEndpoint(maximumHistoryEntriesPerEndpoint);
+                })
+                .AddInMemoryStorage();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapHealthChecksUI(config =>
+                {
+                    config.UIPath = "/hc-ui";                    
+                });
+
+                endpoints.MapHealthChecks("/liveness", new HealthCheckOptions
+                {
+                    Predicate = r => r.Name.Contains("self")
+                });
+            });
+        }
+    }
+}
