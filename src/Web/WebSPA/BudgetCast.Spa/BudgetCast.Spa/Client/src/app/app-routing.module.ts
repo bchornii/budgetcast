0fc8036448b21a2db262a90cee7b6c9730ebf702
Bohdan Chornii <bchornii@softserveinc.com>
2022-01-03 13:03:20 +0200

    migrated to ng11

--- src/Web/WebSPA/BudgetCast.Spa/BudgetCast.Spa/Client/src/app/app-routing.module.ts
+++ src/Web/WebSPA/BudgetCast.Spa/BudgetCast.Spa/Client/src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ export const appRoutes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule]
 })
