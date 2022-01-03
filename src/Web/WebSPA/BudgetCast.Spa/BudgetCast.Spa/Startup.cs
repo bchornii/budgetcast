41ef01dba1370cf917079e2a5289d4aaac1edf3f
Bohdan Chornii <bchornii@softserveinc.com>
2022-01-03 17:47:43 +0200

    Updated cors usage; added middleware to generate CSRF; added middleware to return SPA

--- src/Web/WebSPA/BudgetCast.Spa/BudgetCast.Spa/Startup.cs
+++ src/Web/WebSPA/BudgetCast.Spa/BudgetCast.Spa/Startup.cs
@@ -1,10 +1,13 @@
-using System.IO;
+using System;
 using BudgetCast.Spa.Infrastructure;
 using BudgetCast.Spa.Infrastructure.ExternalUtils;
 using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -17,21 +20,26 @@ namespace BudgetCast.Spa
     {
         public IConfiguration Configuration { get; }
 
+        public Uri UiRootUri { get; }
+
+        public string UiRootUrl { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            UiRootUri = new Uri(Configuration.GetValue<string>("UiBaseUrl"));
+            UiRootUrl = UiRootUri.OriginalString;
         }        
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<AppSettings>(Configuration);
 
-            var uiRoot = Configuration["uiBaseUrl"];
             services.AddCors(options =>
             {
-                options.AddPolicy("CorsPolicy", builder =>
+                options.AddDefaultPolicy(builder =>
                     builder
-                        .WithOrigins(uiRoot)
+                        .WithOrigins(UiRootUrl)
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials());
@@ -46,31 +54,42 @@ namespace BudgetCast.Spa
 
             services.AddHealthChecks()
                 .AddCheck("self", () => HealthCheckResult.Healthy());
+
+            // Setup where the compiled version of our spa application will be, when in production. 
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "wwwroot";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IAntiforgery antiforgery)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-            app.Use(async (context, next) =>
-            {
-                await next();
+            app.UseCors();
 
-                // If there's no available file and the request doesn't contain an extension, we're probably trying to access a page.
-                // Rewrite request to use app root
-                if (context.Response.StatusCode == 404 && 
-                    !Path.HasExtension(context.Request.Path.Value) && 
-                    !context.Request.Path.Value.StartsWith("/api"))
+            // Here we add Angular default Antiforgery cookie name on first load. https://angular.io/guide/http#security-xsrf-protection
+            // This cookie will be read by Angular app and its value will be sent back to the application as the header configured in .AddAntiforgery()
+            app.Use(next => context =>
+            {
+                string path = context.Request.Path.Value;
+
+                if (
+                    string.Equals(path, "/", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(path, "/index.html", StringComparison.OrdinalIgnoreCase))
                 {
-                    context.Request.Path = "/index.html";
-                    context.Response.StatusCode = 200; // Make sure we update the status code, otherwise it returns 404
-                    await next();
+                    // The request token has to be sent as a JavaScript-readable cookie, 
+                    // and Angular uses it by default.
+                    var tokens = antiforgery.GetAndStoreTokens(context);
+                    context.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken,
+                        new CookieOptions() { HttpOnly = false });
                 }
+
+                return next(context);
             });
 
             app.UseDefaultFiles();
@@ -80,7 +99,6 @@ namespace BudgetCast.Spa
                 => opts.EnrichDiagnosticContext = LogEnricher.EnrichFromRequest);
 
             app.UseRouting();
-            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
@@ -96,6 +114,21 @@ namespace BudgetCast.Spa
                     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
                     AllowCachingResponses = false
                 });
+            });
+
+            // Handles all still unnatended (by any other middleware) requests by returning the default page of the SPA (wwwroot/index.html).
+            app.UseSpa(spa =>
+            {
+                // the root of the angular app. (Where the package.json lives)
+                spa.Options.SourcePath = "Client";
+                spa.Options.DevServerPort = UiRootUri.Port;
+
+                if (env.IsDevelopment())
+                {
+
+                    // use the SpaServices extension method for angular, that will make the application to run "ng serve" for us, when in development.
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
             });
         }
     }
