1b2d0175f7184a3c8ddbdcf9677b1a9cedb13844
Bohdan Chornii <bchornii@softserveinc.com>
2022-01-05 18:41:31 +0200

    Added typed id - not used yet

---
+++ src/Common/BudgetCast.Common.Domain/TypedId.cs
@@ -0,0 +1,51 @@
+﻿namespace BudgetCast.Common.Domain
+{
+    public class TypedId : IEquatable<TypedId>
+    {
+        public ulong Value { get; }
+
+        public TypedId(ulong value)
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is TypedId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public bool Equals(TypedId other)
+        {
+            return Value == other.Value;
+        }
+
+        public static bool operator ==(TypedId obj1, TypedId obj2)
+        {
+            if (Equals(obj1, null))
+            {
+                if (Equals(obj2, null))
+                {
+                    return true;
+                }
+                return false;
+            }
+            return obj1.Equals(obj2);
+        }
+        public static bool operator !=(TypedId x, TypedId y)
+        {
+            return !(x == y);
+        }
+
+        public static implicit operator TypedId(ulong value)
+            => new TypedId(value);
+
+        public static implicit operator ulong(TypedId typedId)
+            => typedId.Value;
+    }
+}
---
+++ src/Services/BudgetCast.Expenses/src/BudgetCast.Expenses.Data/Converters/UserTypeConverters.cs
@@ -0,0 +1,15 @@
+﻿using BudgetCast.Common.Domain;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace BudgetCast.Expenses.Data.Converters
+{
+    public static class UserTypeConverters
+    {
+        public static ValueConverter GetTypedIdConverter()
+        {
+            return new ValueConverter<TypedId, ulong>(
+                v => v.Value,
+                v => new TypedId(v));
+        }
+    }
+}
