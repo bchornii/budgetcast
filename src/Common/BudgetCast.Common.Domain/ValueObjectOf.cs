60291dba1c1e290b43e9f2d67b105561c190cb09
Bohdan Chornii <bchornii@softserveinc.com>
2022-02-16 22:27:56 +0200

    Replaced base value object type implementation

---
+++ src/Common/BudgetCast.Common.Domain/ValueObjectOf.cs
@@ -0,0 +1,40 @@
+﻿namespace BudgetCast.Common.Domain
+{
+    public abstract class ValueObjectOf<T>
+        where T : ValueObjectOf<T>
+    {
+        protected abstract bool EqualsCore(T other);
+        protected abstract int GetHashCodeCore();
+
+        public override bool Equals(object obj)
+        {
+            var valueObj = obj as T;
+
+            if (ReferenceEquals(valueObj, null))
+            {
+                return false;
+            }
+            return EqualsCore(valueObj);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        public static bool operator ==(ValueObjectOf<T> left, ValueObjectOf<T> right)
+        {
+            if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ValueObjectOf<T> left, ValueObjectOf<T> right)
+        {
+            return !(left == right);
+        }
+    }
+}
